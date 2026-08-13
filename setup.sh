ad9351faee381067fb5b6255014988a2ba5e7556
Fredrik Klingenberg <fredrkl@gmail.com>
2026-08-13 10:59:23 +0200

    ✨ Thon settings
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- setup.sh
+++ setup.sh
@@ -44,5 +44,6 @@ brew install kubectx
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 kubectl krew install stern
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitconfig-thon ~/.gitconfig-thon
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
